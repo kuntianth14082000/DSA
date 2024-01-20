ea7ee2a0d5d380c8b44515e91705d285fc6d1dc4
Kuntinath Noraje <58073841+kuntianth14082000@users.noreply.github.com>
2024-01-20 16:20:17 +0530

    Create Print matrix row wise.cpp

---
+++ Array/Class/Print matrix row wise.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int student_marks[4][4];
+
+    //take input from user
+    for(int student=0;student<4;student++){
+        for(int subject=0;subject<4;subject++){
+            cin>>student_marks[student][subject];
+        }
+    }
+    //print marks
+    for(int student=0;student<4;student++){
+        for(int subject=0;subject<4;subject++){
+            cout<<student_marks[student][subject]<<" ";
+        }
+        cout<<endl;
+    }
+}
